e0a8682799be36a16270296158cca20c7ff05c90
cmkoivu@comcast.net <cmkoivu@comcast.net>
2017-11-26 07:33:00 -0500

    added bootstrap theme

--- classes/User.php
+++ classes/User.php
@@ -110,7 +110,7 @@
 		    	    user_id INT NOT NULL,
                             role_id INT NOT NULL,
                             FOREIGN KEY (user_id) REFERENCES Users(id),  
-                            FOREIGN KEY (role_id) REFERENCES Roles(id)) 
+                            FOREIGN KEY (role_id) REFERENCES Roles(id))"; 
 			    	   
 		        
 		        $this->query_exec($sql);		            	
---
+++ page.php
@@ -0,0 +1,19 @@
+ 
+     <?php	 
+         require_once('classes/debug.php');  
+         require_once('classes/Request.php');
+         require_once('templates/header.php');
+      ?>
+
+
+
+     <!-- Enter page content here -->
+
+     <div class="container" >
+     
+     </div>
+     <?php	           
+         require_once('templates/footer.php');
+     ?>
+
+
