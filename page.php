6afb19cd0b393872dc32653c4e05bd1bfd81651f
cop4813-UNF <32740287+cop4813-UNF@users.noreply.github.com>
2017-12-01 08:46:45 -0500

    Create page.php

---
+++ page.php
@@ -0,0 +1,17 @@
+
+     <?php	 
+         require_once('classes/debug.php');  
+         require_once('classes/Request.php');
+         require_once('templates/header.php');
+      ?>
+
+
+
+ 
+
+     <div class="container" >        
+        <!-- Enter page content here -->
+     </div>
+     <?php	           
+         require_once('templates/footer.php');
+     ?>
