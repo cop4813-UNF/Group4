7b79a89c273d6adc80ec62c154070634e3d308ea
cop4813-UNF <32740287+cop4813-UNF@users.noreply.github.com>
2017-12-01 16:12:47 -0500

    Create customer_error.php

---
+++ customer_error.php
@@ -0,0 +1,20 @@
+      <?php	 
+         require_once('classes/debug.php');  
+         require_once('classes/Request.php');
+         require_once('templates/header.php');
+      ?>
+
+
+
+ 
+
+     <div class="container" >        
+         <h1>You are not permitted to perform this action</h1>
+
+         <p class="error"> In order to purchase products you need
+         to register or login to you account</p>
+             
+     </div>
+     <?php	           
+         require_once('templates/footer.php');
+     ?>
