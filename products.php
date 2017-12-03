707ecdd09830180cd99c9b082cd3304e3b4197e7
cop4813-UNF <32740287+cop4813-UNF@users.noreply.github.com>
2017-12-03 13:43:22 -0500

    Update products.php
    Products Page now pulls from database and displays neatly on page. no implementation for adding to cart yet

--- products.php
+++ products.php
@@ -9,14 +9,23 @@
 	$req = New Request();
 	$items = $req->get_all_products();
 	
-	print_r(array_values($items));
-	//test code for dsiplaying database. Not Final
-
+	$itemChunks = array_chunk($items,3);
 	
-	//test code for dsiplaying database. Not Final
-
-
-
+	foreach($itemChunks as $chunk)
+	{
+		echo '<div class="row">';
+			foreach($chunk as $product)
+			{
+				echo '<div class="col-sm-4">';
+				echo '<div class="panel panel-primary">';
+				echo '<div class="panel-heading">' .$product['item_name']. '</div>';
+				echo '<div class="panel-body"> <img src="' .$product['image_link']. '" class="img-responsive" style="width:350px;height:250px;" alt="Image"></div>';
+				echo '<div class="panel-body">' .$product['item_description']. '</div>';
+				echo '<div class="panel-footer">' .$product['price']. '</div>';
+				echo '</div></div>';
+			}
+		echo '</div></div></div></br>';
+	}
 ?>
 
 
