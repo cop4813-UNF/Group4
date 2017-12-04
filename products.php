24120ea53b961136c3fa36b3758c3d846c599660
cop4813-UNF <32740287+cop4813-UNF@users.noreply.github.com>
2017-12-03 19:15:24 -0500

    Update products page
    added hidden input type to each item in the footer. WIll hopefully add an add to cart button as well in that location

--- products.php
+++ products.php
@@ -16,12 +16,14 @@
 		echo '<div class="row">';
 			foreach($chunk as $product)
 			{
+				
 				echo '<div class="col-sm-4">';
 				echo '<div class="panel panel-primary">';
 				echo '<div class="panel-heading">' .$product['item_name']. '</div>';
 				echo '<div class="panel-body"> <img src="' .$product['image_link']. '" class="img-responsive" style="width:350px;height:250px;" alt="Image"></div>';
 				echo '<div class="panel-body">' .$product['item_description']. '</div>';
-				echo '<div class="panel-footer">' .$product['price']. '</div>';
+				echo '<div class="panel-footer">' .$product['price'].'</div>';
+				echo '<div class ="panel-footer"> <input type="hidden" name="'.$product['item_name'].'" value="'.$product['item_name'].'"></div>'; 
 				echo '</div></div>';
 			}
 		echo '</div></div></div></br>';
